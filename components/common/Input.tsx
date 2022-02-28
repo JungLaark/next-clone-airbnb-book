25e74d38d786693a85b3c96694925e7f0a9a747d
Chan-Sik Jung <chenone839@gmail.com>
2022-03-01 00:33:57 +0900

    commit

---
+++ components/common/Input.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../../styles/palette";
+
+const Container = styled.div<{iconExist: boolean}>`
+    input{
+
+    }
+`;
+
+interface IProps extends React.InputHTMLAttributes<HTMLInputElement>{
+    icon? : JSX.Element;
+}
+
+const Input: React.FC<IProps> = ({icon, ...props}) => {
+    return (
+        <Container iconExist={!!icon}>
+            <input {...props}/>
+            <div className="input-icon-wrapper">{icon}</div>
+        </Container>
+    );
+}
+
+export default Input;
